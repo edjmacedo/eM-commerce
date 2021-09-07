19e18143c2a25c9e19fb3adc79d9157756b34804
Edvan Macedo <edvan.macedo.jr@gmail.com>
2021-09-07 14:27:10 +0200

    Added header component

--- em-commerce/src/App.js
+++ em-commerce/src/App.js
@@ -3,16 +3,18 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
-  return (
-    <div>
-      <Switch>
-        <Route component={HomePage} path='/' exact />
-        <Route component={ShopPage} path='/shop' />
-      </Switch>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Switch>
+                <Route component={HomePage} path="/" exact />
+                <Route component={ShopPage} path="/shop" />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
