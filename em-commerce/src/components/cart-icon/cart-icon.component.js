8c20276ce3da7de5004808ceb8e3a60a13e5d855
Edvan Macedo <edvan.macedo.jr@gmail.com>
2021-10-27 22:10:57 +0200

    Created cart component with redux store

--- em-commerce/src/components/cart-icon/cart-icon.component.js
+++ em-commerce/src/components/cart-icon/cart-icon.component.js
@@ -1,26 +1,20 @@
 import React from 'react';
-// import { connect } from 'react-redux';
-
-// import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { connect } from 'react-redux';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
 const CartIcon = ({ toggleCartHidden }) => (
-    <div className="cart-icon" /* onClick={toggleCartHidden} */>
+    <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon" />
         <span className="item-count">0</span>
     </div>
 );
 
-export default CartIcon;
-
-/* const mapDispatchToProps = dispatch => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden())
+const mapDispatchToProps = (dispatch) => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(CartIcon); */
+export default connect(null, mapDispatchToProps)(CartIcon);
